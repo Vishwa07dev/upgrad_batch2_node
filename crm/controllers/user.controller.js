b8f8586ab57fc64fceb2baea9c2606c3946571ba
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-09-23 06:19:45 +0530

    class code

---
+++ crm/controllers/user.controller.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * Find all users details - This shoudl be allowed only for the admin
+ */
+exports.findAllUsers = (req, res)=>{
+    
+    // Fetch the data from the DB
+
+     // Remove the private data in these documents
+
+     // Return all the users
+}
+
+
+/**
+ * Find a User based on the userId
+ */
+
+
+/**
+ * Update a user
+ */
